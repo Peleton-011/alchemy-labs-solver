7c1dffa9f6c8d21bfbe1ebd475b6a22cc2946b87
Nico <nicodiazan@gmail.com>
2023-10-22 17:21:03 +0200

    Add some stuff for future calculations

--- src/PotionCell.jsx
+++ src/PotionCell.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const PotionCell = () => {
-	return <div className="potion-cell"></div>;
+const PotionCell = ({ row, col }) => {
+	const parents = [col + 1, col + row + 2];
+	return (
+		<div className="potion-cell">
+			{parents.join(", ")}
+		</div>
+	);
 };
 
 export default PotionCell;
--- src/PotionRow.jsx
+++ src/PotionRow.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import PotionCell from "./PotionCell";
 
-const PotionRow = ({ key, row, len }) => {
+const PotionRow = ({ row, len }) => {
 	return (
 		<div className="potion-row">
 			{(() => {
 				const cells = [];
 				for (let i = 0; i < len-1 ; i++) {
-					cells.push(<PotionCell key={i} row={i} />);
+					cells.push(<PotionCell key={i} row={row} col={i} />);
 				}
 				return cells;
 			})()}
